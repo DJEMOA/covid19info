660942c75b8a9492c009c8be46bd93b41762004f
DJEMO <r.djemo-dessu.15@groupe-esigelec.fr>
2020-03-20 19:17:08 +0100

    data can be send in map view

--- hello.js
+++ hello.js
@@ -2,24 +2,34 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-    // Get data from api
-    var unirest = require("unirest");
-    var req = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");  
+// Get data from api
+var unirest = require("unirest");
+var req = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");  
+var jsonResonse;
         
-    req.headers({
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "357fc5fe18msh5d2faa76fc943fbp1c441djsnf2fd3a28dcd1"
-    });
-
-    req.end(function(res) {
-        if (res.error) throw new Error(res.error);
-            
-        console.log(res.body);
-    });
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/sample1.html'));
+req.headers({
+    "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+    "x-rapidapi-key": "357fc5fe18msh5d2faa76fc943fbp1c441djsnf2fd3a28dcd1"
 });
+
+req.end(function(res) {
+    if (res.error) throw new Error(res.error);
+        
+    jsonResonse = res.body;
+    //console.log(res.body);
+    //create map
+
+});
+
+app.get('/map', function(req, res){
+    res.render('map.ejs',{myData: jsonResonse});
+});
+
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/sample1.html'));
+// });
+
+ 
 
 app.listen(3000);
 
